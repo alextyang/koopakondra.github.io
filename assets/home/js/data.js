5d4b5c4e544593a494a49b82ee778702f78ea5f7
koopakondra <koopakondra@gmail.com>
2020-06-08 18:16:23 -0700

    Added Bookmarks

---
+++ assets/home/js/data.js
@@ -0,0 +1,16 @@
+
+var data_providers = "bookmarks.json";
+  
+$(document).ready(function(){
+  $.getJSON(data_providers,
+    function (data) {
+      var partsource = $('#bookmark-template').html();
+      var parttemplate = Handlebars.compile(partsource);
+      Handlebars.registerPartial('bookmarkTemplate', parttemplate);
+
+      var mysource = $('#section-template').html();
+      var mytemplate = Handlebars.compile(mysource);
+      var myresult = mytemplate(data)
+      $('#bookmarks').html(myresult);
+  });
+});
